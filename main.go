40c21a99cfa1c324611a29580090f215d6b4ea00
Do Xuan Phuong <do.xuan.phuong@thundersoft.com>
2022-09-23 11:33:43 +0700

    - Reorganizing our code, and adding some basic styling to pages - Using bootstrap   https://getbootstrap.com/docs/4.5/getting-started/introduction/

--- main.go
+++ main.go
@@ -2,29 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
-
-// Home í the homepage handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the homepage
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parseTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
-	}
-}
 
 func main() {
 	http.HandleFunc("/", Home)
---
+++ render.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parseTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+	}
+}
