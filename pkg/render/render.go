392171c6d9d6ef8e92afe4287e0f2efd2cb851ff
Do Xuan Phuong <do.xuan.phuong@thundersoft.com>
2022-09-23 16:04:46 +0700

    Working with Layouts

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,7 +8,7 @@ import (
 
 // RenderTemplate using html template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parseTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/"+"base.layout.tmpl")
 	err := parseTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error parsing template:", err)
