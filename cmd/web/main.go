ac8224fc41b2abdf0e99d7b191a11fcaca68fd2f
Do Xuan Phuong <do.xuan.phuong@thundersoft.com>
2022-09-28 15:40:18 +0700

    Section 3: Building a Basic Web Application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/phuongbk90/bookings/pkg/config"
 	"github.com/phuongbk90/bookings/pkg/handlers"
+	"github.com/phuongbk90/bookings/pkg/render"
+	"log"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
-	fmt.Println("Starting application on port: ", portNumber)
-	err := http.ListenAndServe(portNumber, nil)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
 	if err != nil {
-		return
+		log.Fatalln("cannot create template cache")
 	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandler(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+	fmt.Println("Starting application on port: ", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,16 +1,90 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
+	"github.com/phuongbk90/bookings/pkg/config"
+	"github.com/phuongbk90/bookings/pkg/models"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+
+	return td
+}
+
 // RenderTemplate using html template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parseTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/"+"base.layout.tmpl")
-	err := parseTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		// get the template cache from the app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
 	}
+
+	// get requested template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatalln("Could not get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+	td = AddDefaultData(td)
+	_ = t.Execute(buf, td)
+
+	// render the template
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+
+	//parseTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/"+"base.layout.tmpl")
+	//err := parseTemplate.Execute(w, nil)
+	//if err != nil {
+	//	fmt.Println("Error parsing template:", err)
+	//}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := make(map[string]*template.Template)
+
+	// get all the files name *.page.tmpl from ./template
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	//range through all files ending with *>page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
